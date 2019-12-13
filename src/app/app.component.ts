6be726f513ff9468a4d4e626fc833fc36bed093e
Will Worth <will.worth@gmail.com>
2019-12-13 16:29:53 +0100

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'angular-todolist';
+  name: string = 13;
+
+  constructor() {
+    console.log("constructors run when the component is rendered.");
+  }
 }
